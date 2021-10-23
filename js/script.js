4b6abb4c501cfd94bfb42234adb1305ddf10e6ca
callmejogi <kutafin01@gmail.com>
2021-10-23 13:40:47 +0300

    final

--- js/script.js
+++ js/script.js
@@ -1,91 +1,24 @@
-function padString(str, strLength, addSymb, checkAdd = true ){
+function padString(str, strLength, symb, right = true ){
+    if(typeof(str) !== "string") return 'It`s not a string';
+    if(typeof(strLength) !== "number" || isNaN(strLength)) return `strLength is not number`;
+    if(str.length >= strLength) return str.substring(0, strLength);
 
-    if(typeof (str) !== "string"){
-        return `First argument is supposed to be a String!`;
-    };
+    if(typeof(symb) !== "string" && addSymb.length !== 1) return  `symb is invalid`;
+    if(typeof (right) !== "boolean") return `flag is invalid`;
 
-    if(strLength === null || strLength === ""){
-        return `Error!Number of symbols is missing!`;
-    }else if(isNaN(strLength)){
-        return `Error!Second argument is supposed to be a Number!`;
+    let localString = '';
+
+    for(let i = str.length; i < strLength; i++){
+        localString += symb;
     }
 
-    if(addSymb === "" || addSymb === null){
-        return `Error!Here is supposed to be char`;
-    }else if(addSymb === undefined){
-        return str.substr(0, strLength);
-    }else if(addSymb.length !== 1 ){
-        return `Error!Here is supposed to be only one char`;
-    }
-
-    if(strLength > str.length){
-
-        addSymb = addSymb.repeat(strLength - str.length);
-
-        if(checkAdd === true ){
-            return str + addSymb;
-        }else{
-            return addSymb + str;
-        };
-    }else if(strLength < str.length){
-        str = str.substr(0, strLength);
-        return str;
-    }else if(strLength === str.length){
-        return str;
-    };
-
+    return right? str + localString : localString + str;
 
 };
 
+    console.log(padString(prompt('Write a string'), +prompt('Type a number of symbols u want in result'), prompt('Add 1 symbol'), Boolean (prompt('right/left?')) ));
+    console.log(padString(`hello`,2));
+    console.log(padString(`hellooooooo`,20,"%"));
+    console.log(padString(`westtt`,20,"#",false));
+    console.log(padString(`east`,10,"#"));
 
-let str = prompt(`Введите строку:`);
-
-let strLength = +prompt(`Введите желаемое количество символов:`);
-
-let addSymb = prompt(`Введите необходимый символ:`);
-
-let checkAdd = prompt(`Добавить символ справа(true) или слева(false)`);
-
-const result = console.log(padString(str, strLength, addSymb, Boolean(checkAdd)));
-
-console.log(padString(`hello`,8,`*`));
-
-console.log(padString(`hello`,6,`*`,false));
-
-console.log(padString(`hello`,2));
-
-
-//for(i = 0 ;i <= strLength; i++){
-//    if(str.length < strLength){//
-//       return str + addSymb;
-//    };
-//
-//    if(str.length > strLength ){
-//        addSymb = null;
-//        return str.substr(str.length - 1);
-//    }
-//
-//    if(str.length = strLength){
-//        return padString(str, strLength, addSymb = null, checkAdd);
-//    }
-//}
-//}else{
-//for(i = 0 ;i <= strLength; i++){
-//    if(str.length < strLength){
-//        return  addSymb + str;
-//    };
-//}    
-//}
-
-
-
-//prev version
-
-//str = str.substr(0, strLength);
-//addSymb = addSymb.repeat(strLength - str.length);
-
-//if(checkAdd === true ){
-//    return str + addSymb;
-//}else{
-//    return addSymb + str;
-//};
